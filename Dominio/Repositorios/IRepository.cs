c59c8455e4c2aba85f75145d64a6578065e4888a
Carpfede <fedefiasare@gmail.com>
2018-04-18 23:47:21 -0300

    - Se agregaron entidades del dominio - Se agregaron los Framework correspondientes para cada proyecto

---
+++ Dominio/Repositorios/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Repositorios
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+        void Add(TEntity item);
+        void Remove(TEntity item);
+        void Modify(TEntity item);
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetFiltered(Expression<Func<TEntity, bool>> filter);
+        IEnumerable<TEntity> GetAll(params Expression<Func<TEntity, object>>[] includes);
+        void Attach<T>(T item);
+        void Attach<T>(IEnumerable<T> items);
+    }
+}
