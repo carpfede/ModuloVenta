eb12f8238ef60164c15c58253a67fdf8b1670461
Carpfede <fedefiasare@gmail.com>
2018-05-26 21:50:36 -0300

    - Se agrego el repositorio - Se agrego el contexto

---
+++ Datos/Context.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.Entidades;
+using System.Data.Entity;
+
+namespace Datos
+{
+    public class Context : DbContext
+    {
+        public Context() : base("name=LeontinaCalzados")
+        {
+
+        }
+
+        public DbSet<Color> Colores { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<DetalleCompra> DetallesCompras { get; set; }
+        public DbSet<DetalleMercaderia> DetallesMercaderias { get; set; }
+        public DbSet<DetalleVenta> DetallesVentas { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Mercaderia> Mercaderias { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<ProveedorMarca> ProveedoresMarcas { get; set; }
+        public DbSet<RepresentanteProveedor> Representantes { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Talle> Talles { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+    }
+}
---
+++ Datos/Repositorio.cs
@@ -0,0 +1,63 @@
+﻿using Dominio.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Datos
+{
+    public class Repositorio<T> : IRepository<T>, IDisposable
+        where T : class
+    {
+        private readonly Context _context;
+
+        public Repositorio(Context context)
+        {
+            _context = context;
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attach<T1>(T1 item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attach<T1>(IEnumerable<T1> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetFiltered(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modify(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
