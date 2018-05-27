108143eed62513c07e0c04cdf6291a55ba389fe8
Carpfede <fedefiasare@gmail.com>
2018-05-26 21:50:49 -0300

    - Se agrego el menu principal

---
+++ Presentacion/Interfaces/IMenuPrincipal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Presentacion.Interfaces
+{
+    public interface IMenuPrincipal
+    {
+        void CargarFuncionalidad(UserControl control);
+        void CerrarSesion();
+    }
+}
---
+++ Presentacion/Presentadores/MenuPrincipalPresentador.cs
@@ -0,0 +1,14 @@
+﻿using Presentacion.Interfaces;
+
+namespace Presentacion.Presentadores
+{
+    public class MenuPrincipalPresentador
+    {
+        private readonly IMenuPrincipal _view;
+
+        public MenuPrincipalPresentador(IMenuPrincipal view)
+        {
+            _view = view;
+        }
+    }
+}
