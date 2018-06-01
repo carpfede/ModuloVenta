e850a467f7d9b1441810deb4d5d3bdcf5310ee20
Carpfede <fedefiasare@gmail.com>
2018-06-01 02:08:50 -0300

    - Se implementaron los metodos necesarios para la vista de inicio de sesion

--- Presentacion/Vistas/IniciarSesionView.xaml.cs
+++ Presentacion/Vistas/IniciarSesionView.xaml.cs
@@ -9,18 +9,44 @@ namespace Presentacion.Vistas
 
     public partial class IniciarSesionView : Window, IIniciarSesion
     {
+        #region Members
         private IniciarSesionPresentador _presentador;
+        #endregion
 
+        #region Constructor
         public IniciarSesionView(IniciarSesionPresentador presentador)
         {
             _presentador = presentador;
             _presentador.SetView(this);
+            DataContext = this;
             InitializeComponent();
         }
+        #endregion
 
+        #region IIniciarSesion Methods 
+        public string Usuario => txtUsuario.Text;
+        public string Contraseña => txtPass.Password;
         public void AccesoPermitido(Sesion sesion)
         {
             throw new NotImplementedException();
         }
+        #endregion
+
+        #region Private Methods
+        private void BtnMinimize(object sender, EventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnIngresar(object sender, EventArgs e)
+        {
+            _presentador.IngresarAlSistema();
+        }
+        #endregion
     }
 }
