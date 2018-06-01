d7f198dd13550627435fa6572f20b57869ac469c
Carpfede <fedefiasare@gmail.com>
2018-06-01 02:09:43 -0300

    - Se agrego inyeccion del caso de uso inicio sesion

--- Presentacion/Presentadores/IniciarSesionPresentador.cs
+++ Presentacion/Presentadores/IniciarSesionPresentador.cs
@@ -1,14 +1,27 @@
-﻿using Presentacion.Interfaces;
+﻿using System;
+using Presentacion.Interfaces;
+using Aplicacion.CasosDeUso.Interfaces;
 
 namespace Presentacion.Presentadores
 {
     public class IniciarSesionPresentador : IPresenter<IIniciarSesion>
     {
         private IIniciarSesion _view;
+        private IInicioSesion _inicioSesion;
+
+        public IniciarSesionPresentador(IInicioSesion inicioSesion)
+        {
+            _inicioSesion = inicioSesion;
+        }
 
         public void SetView(IIniciarSesion view)
         {
             _view = view;
+        }
+
+        public void IngresarAlSistema()
+        {
+            _inicioSesion.AutenticarUsuario(_view.Usuario, _view.Contraseña);
         }
     }
 }
