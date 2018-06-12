84e753bc9449fbe39578793ba3c14fa2bd128274
Carpfede <fedefiasare@gmail.com>
2018-06-12 00:57:58 -0300

    Servicio proveedor de funcionalidades

---
+++ Aplicacion/Servicios/IFuncionalidadService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Aplicacion.Servicios
+{
+    public interface IFuncionalidadService
+    {
+        List<MenuItemBase> Funcionalidades(List<string> roles);
+    }
+}
