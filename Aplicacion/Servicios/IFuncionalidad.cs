11b8e92e536003fdd45bb53a000317df06d76dee
Carpfede <fedefiasare@gmail.com>
2018-06-12 00:56:41 -0300

    Se agrego un item comun correspondiente a un caso de uso

--- Aplicacion/Servicios/IFuncionalidad.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Aplicacion.Servicios
-{
-    public interface IFuncionalidad
-    {
-        string Nombre { get; set; }
-        object Content { get; set; }
-    }
-}
---
+++ Aplicacion/Servicios/MenuItemBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Aplicacion.Servicios
+{
+    public class MenuItemBase
+    {
+        public string Nombre { get; set; }
+        public object Content { get; set; }
+    }
+}
