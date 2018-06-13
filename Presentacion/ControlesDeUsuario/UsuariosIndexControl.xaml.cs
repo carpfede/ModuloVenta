80bfa9e20a5a43975532682190c2e7262915d14f
Carpfede <fedefiasare@gmail.com>
2018-06-13 00:44:12 -0300

    Listado de todos los usuarios

---
+++ Presentacion/ControlesDeUsuario/UsuariosIndexControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Dominio.Entidades;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Windows.Controls;
+
+namespace Presentacion.ControlesDeUsuario
+{
+    [PrincipalPermission(SecurityAction.Demand, Role = "Administrador")]
+    public partial class UsuariosIndexControl : UserControl
+    {
+        public List<Usuario> Lista { get; }
+        public UsuariosIndexControl(List<Usuario> usuarios)
+        {
+            Lista = usuarios;
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
